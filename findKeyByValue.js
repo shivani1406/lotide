208a61bc9617b67a6aab90e179f97312d98dc632
Shivani Sharma <shivani1406sharma@gmail.com>
2021-11-24 21:45:18 +0000

    new commit

---
+++ findKeyByValue.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  if(actual == expected)
+  {
+    console.log(`Assertion Passed: ${actual} = ${expected}`);
+  }
+  else
+  {
+    console.log(`Assertion Failed: ${actual} != ${expected}`);
+  }
+  //console.assert((actual===expected) ,"Lighthouse Labs != Bootcamp");
+};
+
+const findKeyByValue = function(obj,val){
+let key="";
+let result="";
+for(let keys in obj)
+{
+  key=obj[keys];
+  // console.log(key);
+  if(key === val)
+  {
+    result = keys;
+  }
+}
+return result;
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
