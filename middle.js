5cb20b420db10312f458ad29e078b9a60f22ac95
Shivani Sharma <shivani1406sharma@gmail.com>
2021-11-24 16:37:57 +0000

    new commit

--- middle.js
+++ middle.js
@@ -29,19 +29,30 @@ const assertArraysEqual = function (actual, expected){
 
 }
 
-const without = function(original, remove){
-  let newarr = [];
-  for(let i = 0; i < original.length; i++)
-  {
-    if(original[i] != remove)
-    {
-      newarr.push(original[i]);
-    }
-  }
-  console.log(newarr);
+const middle = function(array) {
+  //...
+  let midarray = [];
+if(array.length >= 3)
+{
+let count = array.length;
+let half = Math.ceil(count/2);
+if(count % 2 == 0)
+{
+for(let i=half-1; i<=half; i++)
+{
+  midarray.push(array[i]);
+}
+}
+else{
+midarray.push(array[half-1]);
+}
+}
+return console.log(midarray);
 }
 
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+middle([1]) // => []
+middle([1, 2]) // => []
+middle([1, 2, 3]) // => [2]
+middle([1, 2, 3, 4, 5]) // => [3]
+middle([1, 2, 3, 4]) // => [2, 3]
+middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
