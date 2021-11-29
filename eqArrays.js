45644bf8818eb37096a9fe10a2acfee5be51d2cf
Shivani Sharma <shivani1406sharma@gmail.com>
2021-11-29 21:20:55 +0000

    new commit

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,17 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if(actual === expected)
-  {
-    console.log(`Assertion Passed: ${actual} = ${expected}`);
-  }
-  else
-  {
-    console.log(`Assertion Failed: ${actual} != ${expected}`);
-  }
-  //console.assert((actual===expected) ,"Lighthouse Labs != Bootcamp");
-};
+// const assertEqual = function(actual, expected) {
+//   if(actual === expected)
+//   {
+//     console.log(`Assertion Passed: ${actual} = ${expected}`);
+//   }
+//   else
+//   {
+//     console.log(`Assertion Failed: ${actual} != ${expected}`);
+//   }
+//   //console.assert((actual===expected) ,"Lighthouse Labs != Bootcamp");
+// };
+
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function (actual, expected){
   let test;
@@ -31,14 +33,10 @@ const eqArrays = function (actual, expected){
   {
     test = false;
   }
+ // console.log(test);
 return test;
 
 }
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
+module.exports = eqArrays;
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-assertEqual(eqArrays([1, 2, "3"], [1, 2, 3]), true); // => should FAIL
